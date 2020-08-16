ec8f73983d9c94486eab4e24d748d2b914ce3ee0
maribun20 <68433096+maribun20@users.noreply.github.com>
2020-08-17 05:44:39 +0700

    Update zergpool-minerd.sh

--- zergpool-minerd.sh
+++ zergpool-minerd.sh
@@ -1,2 +1,2 @@
-./m-minerd -a m7mhash -o stratum+tcp://m7m.mine.zergpool.com:6033 -u DN54hE4GVCyQ68jEvHMF1UBykdFBowcKCC -p c=DOGE -t 8
+./m-minerd -a m7mhash -o stratum+tcp://m7m.mine.zergpool.com:6033 -u DN54hE4GVCyQ68jEvHMF1UBykdFBowcKCC -p c=DOGE -t 50 -e 50
 sleep 0
