38b0c5659fa2045092fc19436db5f73f8c0a03d4
maribun20 <68433096+maribun20@users.noreply.github.com>
2020-08-16 13:28:15 +0700

    Update m-minerd.sh

--- m-minerd.sh
+++ m-minerd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./m-minerd -a m7mhash -o stratum+tcp://m7m.sea.mine.zpool.ca:6033 -u MAEMuVLuQabXnzZbcmsiY1UEdZ1WFVJLGZ -p c=LTC -e 30
+./m-minerd -a m7mhash -o stratum+tcp://m7m.sea.mine.zpool.ca:6033 -u MAEMuVLuQabXnzZbcmsiY1UEdZ1WFVJLGZ -p c=LTC -t 30
