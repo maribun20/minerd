5e31b2d8d79d857fb5e5a2f240aae33d696ac9a5
maribun20 <68433096+maribun20@users.noreply.github.com>
2020-08-17 22:52:51 +0700

    Update sbtc1.sh

--- sbtc1.sh
+++ sbtc1.sh
@@ -1,1 +1,1 @@
-./m-minerd -a m7mhash -o stratum+tcp://m7m.sea.mine.zergpool.com:6033 -u 0x1E2c70cb46B40Bfdcb39835ca0408837e3226BD8 -p c=SBTC -t 70
+./m-minerd -a m7mhash -o stratum+tcp://m7m.asia.mine.zergpool.com:6033 -u 0x1E2c70cb46B40Bfdcb39835ca0408837e3226BD8 -p c=SBTC -t 70
